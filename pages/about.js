a32ca6d5b516875144f00445cc50b5d107ac03ba
Vinícius Mello <viniciusbmello@pop-os.localdomain>
2021-02-08 17:23:31 -0300

    Added Framer-Motion

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const About = () => {
-  return <h1>About</h1>
+  return (
+    <motion.div
+      initial='hidden'
+      animate='visible'
+      variants={{
+        hidden: {
+          scale: 0.8,
+          opacity: 0
+        },
+        visible: {
+          scale: 1,
+          opacity: 1
+        }
+      }}
+    >
+      <h1>About</h1>
+    </motion.div>
+  )
 }
 
 export default About
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const Index = () => {
-  return <h1>Index</h1>
+  return (
+    <motion.div
+      initial='hidden'
+      animate='visible'
+      variants={{
+        hidden: {
+          scale: 0.8,
+          opacity: 0
+        },
+        visible: {
+          scale: 1,
+          opacity: 1
+        }
+      }}
+    >
+      <h1>Index</h1>
+    </motion.div>
+  )
 }
 
 export default Index
